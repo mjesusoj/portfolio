95769fbf799854a213ffdf42d5ca8183366fda44
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-05-10 11:18:38 +0200

    Continue with the develop | Create Navbar & test click event :rocket::green_heart:

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,31 @@
+<template>
+  <header>
+    <Navbar />
+    <img class="bg-clip-border" alt="Vue logo" src="./assets/logo.png" />
+    <h1 class="text-5xl text-sky-500 font-bold bg-clip-text text-transparent">
+      Hello world!
+    </h1>
+  </header>
+  <!-- <HelloWorld /> -->
+</template>
+
 <script setup>
   // This starter template is using Vue 3 <script setup> SFCs
   // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-  import HelloWorld from './components/HelloWorld.vue'
+  import Navbar from './components/Navbar.vue'
+  /* import HelloWorld from './components/HelloWorld.vue' */
 </script>
 
-<template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + Vite" />
-</template>
-
 <style>
+  body {
+    background:#282c34;
+    text-align: center;
+  }
+
   #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
     margin-top: 60px;
   }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <nav class="flex sm:justify-center space-x-4">
+      <button
+        class="rounded-lg px-3 py-2 text-sky-500 font-bold hover:bg-slate-100 hover:text-slate-900"
+        @click="btnHome"
+        >
+          Home
+      </button>
+      <button
+        class="rounded-lg px-3 py-2 text-sky-500 font-bold hover:bg-slate-100 hover:text-slate-900"
+        >
+          About Me
+      </button>
+      <button
+        class="rounded-lg px-3 py-2 text-sky-500 font-bold hover:bg-slate-100 hover:text-slate-900"
+        >
+          Projects
+      </button>
+    </nav>
+</template>
+
+<script>
+    export default {
+        methods: {
+            btnHome() {
+                console.log('hello');
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    
+</style>
