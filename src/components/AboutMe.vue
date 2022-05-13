379cd7c666d1a85ce3e9513245c345f46d40efa9
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-05-13 09:39:24 +0200

    Fix responsive photo of about-me #1 :rocket:

--- src/components/AboutMe.vue
+++ src/components/AboutMe.vue
@@ -1,40 +1,33 @@
 <template>
-    <figure id="about-me" class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+    <figure id="about-me" class="lg:flex bg-slate-100 rounded-lg p-8 lg:p-0 dark:bg-slate-800">
         <img class="w-25 h-24 md:w-4/12 md:h-full xl:w-auto xl:h-auto md:rounded-md rounded-full mx-auto" src="../assets/photo-mj.png" alt="Vue logo"  />
         <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
-        <blockquote>
-            <h2 class="text-3xl text-sky-400 font-bold bg-clip-text">
-                About Me
-            </h2>
-            <p class="text-sky-200 font-medium mt-2">
-                Me gusta la tecnología desde los 16 años, que es cuando comencé el Grado Medio de SMR.
-                <br />
-                En ese momento, me encontré con el mundo de la tecnología, y cuando cursaba el segundo año, 
-                me interesé por el desarrollo web.
-                <br />
-                Cuando finalicé el grado medio, supe que quería estudiar el Grado Superior de DAW y desde ese momento,
-                empecé a aprender las tecnologías necesarias para desarrollar aplicaciones web y 
-                a profundizar en el mundo de los frameworks, por mi cuenta, con ayuda de cursos online, hasta hoy.
-            </p>
-        </blockquote>
-        <figcaption class="font-bold">
-            <div class="text-sky-500 dark:text-sky-400">
-                Manuel Jesús Ojeda
-            </div>
-            <div class="text-slate-600 dark:text-slate-400">
-                Front-End Developer 👨‍💻 | Desarrollador web 
-            </div>
-        </figcaption>
+            <blockquote>
+                <h2 class="text-3xl text-sky-400 font-bold bg-clip-text">
+                    About Me
+                </h2>
+                <p class="text-sky-200 font-medium mt-2">
+                    Me gusta la tecnología desde los 16 años, que es cuando comencé el Grado Medio de SMR.
+                    <br />
+                    En ese momento, me encontré con el mundo de la tecnología, y cuando cursaba el segundo año, 
+                    me interesé por el desarrollo web.
+                    <br />
+                    Cuando finalicé el grado medio, supe que quería estudiar el Grado Superior de DAW y desde ese momento,
+                    empecé a aprender las tecnologías necesarias para desarrollar aplicaciones web y 
+                    a profundizar en el mundo de los frameworks, por mi cuenta, con ayuda de cursos online, hasta hoy.
+                </p>
+            </blockquote>
+            <figcaption class="font-bold">
+                <div class="text-sky-500 dark:text-sky-400">
+                    Manuel Jesús Ojeda
+                </div>
+                <div class="text-slate-600 dark:text-slate-400">
+                    Front-End Developer 👨‍💻 | Desarrollador web 
+                </div>
+            </figcaption>
         </div>
     </figure>
 </template>
 
 <script>
-    export default {
-        name: 'name'
-    }
 </script>
-
-<style>
-
-</style>
