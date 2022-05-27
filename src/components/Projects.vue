4977726b9d03a67d9ee34941ffd448fc926d61dc
mjesusoj <m.jesusojedasalvador@gmail.com>
2022-05-27 12:25:48 +0200

    Finish Redisign of Projects #1 :rocket:

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -13,54 +13,132 @@
                 <a href="https://craftianosmoney.com" target="_blank" rel="noopener noreferrer">
                     <h3 class="text-2xl text-sky-500 hover:text-sky-700 font-bold bg-clip-text">CraftianosMoney Page</h3>
                 </a>
-                <!-- <p class="font-medium mt-2">
-                    Este proyecto me ha permitido conocer más de las tecnologías que se utilizan en el desarrollo de páginas web.
-                </p> -->
                 <ul class="list-disc list-inside text-left p-3">
-                    <li class="mt-1">Es el proyecto <i>que más completo tengo, ya que se convirtió en un reto personal</i></li>
-                    <li class="mt-1">Desarrollado con <b>React.js</b> y uso de <b>react-router</b> para el manejo de rutas ✅</li>
-                    <li class="mt-1">Uso de frameworks CSS como <b>Bootstrap</b> y <b>SCSS</b> como preprocesador ✅</li>
-                    <li class="mt-1">Implementación de <b>técnicas SEO</b> para el posicionamiento en buscadores ✅</li>
+                    <li class="mt-2">Es el proyecto <i>que más completo tengo, ya que se convirtió en un reto personal</i></li>
+                    <li class="mt-2">Desarrollado con <b>React.js</b> y uso de <b>react-router</b> para el manejo de rutas 🔗</li>
+                    <li class="mt-2">Uso de frameworks CSS como <b>Bootstrap</b> y <b>SCSS</b> como preprocesador ✅</li>
+                    <li class="mt-2">Desplegado en <b>hosting independiente y dominio personalizado</b> ✨</li>
+                    <li class="mt-2">Implementación de <b>técnicas SEO</b> para el posicionamiento en buscadores webs ✅</li>
                 </ul>
                 <div class="tech mt-2">
-                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">HTML</button>
-                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">CSS</button>
-                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">JS</button>
-                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">React JS</button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        HTML
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        CSS
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        JS
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        React.js
+                    </button>
                 </div>
             </div>
             <div class="pokemonGame">
                 <a href="https://pokemon-game-ten.vercel.app/" target="_blank" rel="noopener noreferrer">
-                    <h3 class="text-2xl text-sky-500 hover:text-sky-700 font-bold bg-clip-text">Pokemon Game</h3>
+                    <h3 class="text-2xl text-sky-500 hover:text-sky-700 font-bold bg-clip-text pt-2">Pokemon Game</h3>
                 </a>
-                <p class="font-medium mt-2">
-                    Este proyecto pertenece al curso de 
-                    <a class="text-orange-300 hover:text-orange-400 font-bold" href="https://www.udemy.com/course/vuejs-fh/" target="_blank" rel="noopener noreferrer">
-                        Vue.js: De cero a experto de Udemy
-                    </a>,
-                    es un pequeño juego de pokemon, el cual está desarrollado con Vue.js, manejo de rutas con Vue Router.
-                    El listado de pokemons, se recogen de una API externa y se muestran en una lista, con la ayuda de Vuex.
-                    Además de usar Bootstrap como framework de CSS, FontAwesome para los iconos, animaciones y fuentes personalizadas.
-                </p>
+                <ul class="list-disc list-inside text-left p-3">
+                    <li class="mt-2">
+                        Este proyecto está incluido en el curso de Udemy: <br />
+                        <a class="text-orange-300 hover:text-orange-400 font-bold" 
+                            href="https://www.udemy.com/course/vuejs-fh/" target="_blank" rel="noopener noreferrer">
+                            Vue.js: De cero a experto
+                        </a>
+                    </li>
+                    <li class="mt-2">
+                        Desarrollado con Vue.js y uso de <b>Vuex</b> para el manejo de datos ✅
+                    </li>
+                    <li class="mt-2">
+                        El listado de pokemons se recoge de una API externa
+                        y se muestra en una lista, <br /> con la ayuda de Vuex
+                    </li>
+                    <li class="mt-4">
+                        Uso de FontAwesome para mostrar iconos personalizados, animaciones, 
+                        además de incluir una fuente acorde al juego
+                    </li>
+                </ul>
+                <div class="tech mt-2">
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        HTML
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        CSS
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        JS
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        Vue.js
+                    </button>
+                </div>
             </div>
             <div class="app-tailwind">
-                <a href="https://github.com/mjesusoj/my-firstapp-tailwind" target="_blank" rel="noopener noreferrer">
-                    <h3 class="text-2xl text-sky-500 hover:text-sky-700 font-bold bg-clip-text">My First App with Tailwind</h3>
+                <a href="https://my-firstapp-tailwind.vercel.app" target="_blank" rel="noopener noreferrer">
+                    <h3 class="text-2xl text-sky-500 hover:text-sky-700 font-bold bg-clip-text pt-2">Learning Tailwind</h3>
                 </a>
-                <p class="font-medium mt-2">
-                    Proyecto sencillo, implementando por primera vez Tailwind CSS, con React.js, 
-                    utilizando manejo de rutas con React Router, y utilizando una librería de Tailwind para el formulario.
-                </p>
+                <ul class="list-disc list-inside text-left p-3">
+                    <li class="mt-2">
+                        Proyecto personal para aprender a usar Tailwind CSS por primera vez 😍
+                    </li>
+                    <li class="mt-2">
+                        Desarrollado con React.js y uso de <b>react-router</b> para el manejo de rutas ✅
+                    </li>
+                </ul>
+                <div class="tech mt-2">
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        HTML
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        CSS
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        JS
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        React.js
+                    </button>
+                </div>
             </div>
             <div class="vacacionesCody">
                 <a href="https://mjesusoj.github.io/vacaciones-cody/" target="_blank" rel="noopener noreferrer">
                     <h3 class="text-2xl text-sky-500 hover:text-sky-700 font-bold bg-clip-text">Vacaciones Cody</h3>
                 </a>
-                <p class="font-medium mt-2">
-                    Con este proyecto se partía de una base construida con HTML y CSS, 
-                    para más tarde aplicarle Bootstrap como framework de CSS, 
-                    botón para cambiar de Modo oscuro/claro y para las fuentes, el recurso gratuito "Google Fonts".
-                </p>
+                <ul class="list-disc list-inside text-left p-3">
+                    <li class="mt-2">
+                        Reto de transformar una página inicial con HTML y CSS a implementarla con Bootstrap
+                    </li>
+                    <li class="mt-2">
+                        Implementación de fuentes personalizadas, uso del recurso "Google Fonts"
+                    </li>
+                </ul>
+                <div class="tech mt-2">
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        HTML
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        CSS
+                    </button>
+                    <button class="py-2 px-3 mx-1 bg-blue-500 text-white font-semibold rounded-lg shadow-md 
+                        hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+                        JS
+                    </button>
+                </div>
             </div>
         </div>
     </div>
